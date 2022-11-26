441b22da9223b9c97b2cb27ae80d8d2e5b5d8e88
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-11-26 22:26:45 +0300

    fixed

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,20 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
+        case 'sort':
+            const newState = [...state];
+            newState.sort(function (a, b) {
+                if (action.payload === 'up') {
+                    return a.name > b.name ? 1 : -1
+                } else if (action.payload === 'down') {
+                    return a.name < b.name ? 1 : -1
+                } else return 0
+            })
+            return newState;
         case 'check': {
-
-            return state // need to fix
+            return state.filter(user => user.age > 18)
         }
         default:
             return state
