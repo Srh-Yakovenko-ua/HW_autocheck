3e4d09f85630b73479835be49ac1c3ea0201590e
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-12-05 14:08:49 +0300

    fixed

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,17 @@
-const initState = {
-    themeId: 1,
-}
+interface changeThemeIdType {type: 'SET_THEME_ID', id: number}
+interface initStateType {themeId: number}
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+const initState = {themeId: 1,}
+
+export const themeReducer = (state: initStateType = initState, action: changeThemeIdType): initStateType => { // fix any
     switch (action.type) {
-        // дописать
-
+        case 'SET_THEME_ID' :
+            return {
+                themeId: action.id
+            }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): changeThemeIdType => ({type: 'SET_THEME_ID', id}) // fix any
